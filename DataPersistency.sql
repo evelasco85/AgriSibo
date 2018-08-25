f543386ca815ab1c8c9963532139a687cb79718d
Enrico Velasco <enrico_velasco@hotmail.com>
2018-08-26 01:30:20 +0800

    Added schema

--- DataPersistency.sql
+++ DataPersistency.sql
@@ -1,14 +1,28 @@
-EXEC	[AgriSibo].[dbo].[AddInvestment]
+/*
+EXEC	[AgriSibo].[dbo].SubmitInvestmentApplication
 		@InvestorId = 1,	--Enrico
 		@ProjectId = 1,
 		@InvestedAmount = 80,
 		@ExpectedReturnRate = 0,
 		@ExpectedReturnAmount = 110
 
-
-EXEC	[AgriSibo].[dbo].[AddInvestment]
+EXEC	[AgriSibo].[dbo].SubmitInvestmentApplication
 		@InvestorId = 2,	--Jonel
 		@ProjectId = 1,
 		@InvestedAmount = 70,
 		@ExpectedReturnRate = 0,
 		@ExpectedReturnAmount = 110
+
+EXEC	[AgriSibo].[dbo].AcceptInvestmentApplication
+		@ProjectId = 1,
+		@InvestorId = 1
+
+--Escrow to Farmer
+EXEC	[AgriSibo].[dbo].[InitiateProject] @ProjectId = 1
+
+--Add funds to Farmer
+EXEC	[AgriSibo].[dbo].[AddFundsToFarmer] @id = 1, @fundsToAdd = 200
+
+--Add funds to Farmer
+EXEC	[AgriSibo].[dbo].[CompleteProject] @ProjectId = 1
+*/
