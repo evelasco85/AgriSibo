f2567abde5fb2b19e61744caaef9722da650e36c
Enrico Velasco <enrico_velasco@hotmail.com>
2018-08-25 17:09:45 +0800

    Added basic controllers

---
+++ WebApi/AgriSibo.WebApi/AgriSibo.WebApi/UnionBank/Security.cs
@@ -0,0 +1,21 @@
+﻿using AgriSibo.WebApi.EF;
+using AgriSibo.WebApi.Utilities;
+using System.Linq;
+
+namespace AgriSibo.WebApi.UnionBank
+{
+    public static class Security
+    {
+        public static string GetToken(int entityId)
+        {
+            ApiSecurity record = Data
+                .Entities
+                .ApiSecurities
+                .FirstOrDefault(security => security.entityId == entityId);
+
+            if (record == null) return string.Empty;
+
+            return string.Empty;
+        }
+    }
+}
