ff4ce46ebfb75b4c0b4a7cd353714b353e074b03
Enrico Velasco <enrico_velasco@hotmail.com>
2018-08-25 21:59:13 +0800

    --amend

---
+++ DataPersistency.sql
@@ -0,0 +1,14 @@
+EXEC	[AgriSibo].[dbo].[AddInvestment]
+		@InvestorId = 1,	--Enrico
+		@ProjectId = 1,
+		@InvestedAmount = 80,
+		@ExpectedReturnRate = 0,
+		@ExpectedReturnAmount = 110
+
+
+EXEC	[AgriSibo].[dbo].[AddInvestment]
+		@InvestorId = 2,	--Jonel
+		@ProjectId = 1,
+		@InvestedAmount = 70,
+		@ExpectedReturnRate = 0,
+		@ExpectedReturnAmount = 110
