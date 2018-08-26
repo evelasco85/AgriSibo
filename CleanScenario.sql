6b2d5b2bd555138ae1e48eaf7da3f4cb88586f0c
Enrico Velasco <enrico_velasco@hotmail.com>
2018-08-26 10:45:03 +0800

    --amend

--- CleanScenario.sql
+++ CleanScenario.sql
@@ -1,39 +1,39 @@
 delete from [AgriSibo].dbo.Investment
 
 /*
-delete from [AgriSibo].dbo.Project
+--delete from [AgriSibo].dbo.Project
 
-EXEC	[AgriSibo].[dbo].[AddProject]
-		@farmersCropId = 3,
-		@name = 'September Cycle (Durian)',
-		@estimatedBudget = 15000,
-		@committedDate = '2018-12-25',
-		@defaultReturnRate = '0',
-		@defaultReturnAmount = 17500.00
+--EXEC	[AgriSibo].[dbo].[AddProject]
+--		@farmersCropId = 3,
+--		@name = 'September Cycle (Durian)',
+--		@estimatedBudget = 15000,
+--		@committedDate = '2018-12-25',
+--		@defaultReturnRate = '0',
+--		@defaultReturnAmount = 17500.00
 
-EXEC	[AgriSibo].[dbo].[AddProject]
-		@farmersCropId = 4,
-		@name = 'September Cycle (Cacao)',
-		@estimatedBudget = 8000,
-		@committedDate = '2018-12-25',
-		@defaultReturnRate = 0,
-		@defaultReturnAmount = 12000
+--EXEC	[AgriSibo].[dbo].[AddProject]
+--		@farmersCropId = 4,
+--		@name = 'September Cycle (Cacao)',
+--		@estimatedBudget = 8000,
+--		@committedDate = '2018-12-25',
+--		@defaultReturnRate = 0,
+--		@defaultReturnAmount = 12000
 
-EXEC	[AgriSibo].[dbo].[AddProject]
-		@farmersCropId = 5,
-		@name = 'September Cycle (Mango)',
-		@estimatedBudget = 7000,
-		@committedDate = '2018-12-25',
-		@defaultReturnRate = 0,
-		@defaultReturnAmount = 8500
+--EXEC	[AgriSibo].[dbo].[AddProject]
+--		@farmersCropId = 5,
+--		@name = 'September Cycle (Mango)',
+--		@estimatedBudget = 7000,
+--		@committedDate = '2018-12-25',
+--		@defaultReturnRate = 0,
+--		@defaultReturnAmount = 8500
 
-EXEC	[AgriSibo].[dbo].[AddProject]
-		@farmersCropId = 6,
-		@name = 'September Cycle (Banana)',
-		@estimatedBudget = 7000,
-		@committedDate = '2018-12-25',
-		@defaultReturnRate = 0,
-		@defaultReturnAmount = 8500
+--EXEC	[AgriSibo].[dbo].[AddProject]
+--		@farmersCropId = 6,
+--		@name = 'September Cycle (Banana)',
+--		@estimatedBudget = 7000,
+--		@committedDate = '2018-12-25',
+--		@defaultReturnRate = 0,
+--		@defaultReturnAmount = 8500
 */
 
 update AgriSibo.dbo.Investor set RemainingInvestmentFunds = 25000 where id = 1
--- DataPersistency.sql
+++ DataPersistency.sql
@@ -1,4 +1,5 @@
 /*
+--Step 2.1
 EXEC	[AgriSibo].[dbo].SubmitInvestmentApplication
 		@InvestorId = 1,	--Enrico
 		@ProjectId = 17,
@@ -6,6 +7,7 @@ EXEC	[AgriSibo].[dbo].SubmitInvestmentApplication
 		@ExpectedReturnRate = 0,
 		@ExpectedReturnAmount = 12500
 
+--Step 2.2
 EXEC	[AgriSibo].[dbo].SubmitInvestmentApplication
 		@InvestorId = 2,	--Jonel
 		@ProjectId = 17,
@@ -13,16 +15,20 @@ EXEC	[AgriSibo].[dbo].SubmitInvestmentApplication
 		@ExpectedReturnRate = 0,
 		@ExpectedReturnAmount = 10000
 
+--Step 3
 EXEC	[AgriSibo].[dbo].AcceptInvestmentApplication
 		@ProjectId = 17,
 		@InvestorId = 1
 
+--Step 4
 --Escrow to Farmer
 EXEC	[AgriSibo].[dbo].[InitiateProject] @ProjectId = 17
 
+--Step 5: Prepare payments to investors
 --Add funds to Farmer
 EXEC	[AgriSibo].[dbo].[AddFundsToFarmer] @id = 1, @fundsToAdd = 10000
 
+--Step 6: Pay to investors
 --Complete the project
 EXEC	[AgriSibo].[dbo].[CompleteProject] @ProjectId = 17
 */
